8193976f445d8ddfff27fe44fc9f543ec7d44d42
maxim <maksgoy2911@gmail.com>
2023-05-14 19:48:57 +0300

    update zapiska

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { MainPage } from "./pages/Main";
+import { MainPage } from "./pages/MainPage/Main";
 import { AboutPage } from "./pages/About";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { AdminPage } from "./pages/AdminPage/AdminPage";
