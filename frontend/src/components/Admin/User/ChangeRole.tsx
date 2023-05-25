7decbc22915fbda4369259e66ebde08bb1f53720
maxim <maksgoy2911@gmail.com>
2023-05-25 20:50:45 +0300

    search teams

--- frontend/src/components/Admin/User/ChangeRole.tsx
+++ frontend/src/components/Admin/User/ChangeRole.tsx
@@ -1,47 +1,80 @@
 import { useState } from "react";
 import { IUser } from "../../../entity/User";
 import axios, { AxiosError } from "axios";
+import styled from "styled-components";
 
 interface Props {
-    user: IUser;
+  user: IUser;
 }
 
 export function ChangeRole({ user }: Props) {
-    const [button, setButton] = useState("Изменить");
-    const [errorMessage, setErrorMessage] = useState("");
-    const [activeButton, setActiveButton] = useState(false);
-    const [role, setRole] = useState("admin")
+  const [button, setButton] = useState("Изменить");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [activeButton, setActiveButton] = useState(false);
+  const [role, setRole] = useState("admin");
 
-    async function Change() {
-        setButton("Изменение...");
+  async function Change() {
+    setButton("Изменение...");
 
-        try {
-            const response = await axios.put(
-                "https://localhost:7167/api/User/ChangeRole/" + user.id + "/" + role
-            )
-            window.location.assign("/admin/users")
-        } catch (e: unknown) {
-            const error = e as AxiosError;
-            // console.log(error.message);
-            // console.log(error.response?.data);
-            const message = error.response?.data as String;
-            setErrorMessage(message.toString());
-            console.log(error)
-        }
-        setButton("Изменить")
+    try {
+      const response = await axios.put(
+        "https://localhost:7167/api/User/ChangeRole/" + user.id + "/" + role
+      );
+      window.location.assign("/admin/users");
+    } catch (e: unknown) {
+      const error = e as AxiosError;
+      // console.log(error.message);
+      // console.log(error.response?.data);
+      const message = error.response?.data as String;
+      setErrorMessage(message.toString());
+      console.log(error);
     }
+    setButton("Изменить");
+  }
 
-    return (<div>
-        <div>Изменить роль {user.email}</div>
-        <div>
-            <select onChange={(e) => setRole(e.target.value)}>
-                <option value="admin">Администратор</option>
-                <option value="user">Пользователь</option>
-                <option value="bukmeker">Букмекер</option>
-            </select>
-        </div>
-        <div>
-            <button disabled={activeButton} onClick={Change}>{button}</button>
-        </div>
-    </div>)
+  return (
+    <FormContainer>
+      <Div>Изменить роль {user.email}</Div>
+      <Div>
+        <select onChange={(e) => setRole(e.target.value)}>
+          <option value="admin">Администратор</option>
+          <option value="user">Пользователь</option>
+          <option value="bukmeker">Букмекер</option>
+        </select>
+      </Div>
+      <Div>
+        <StyledButton disabled={activeButton} onClick={Change}>
+          {button}
+        </StyledButton>
+      </Div>
+    </FormContainer>
+  );
 }
+
+const FormContainer = styled.div`
+  font-size: 18px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const StyledButton = styled.button`
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  height: 56px;
+  padding-left: 60px;
+  padding-right: 60px;
+  font-size: 18px;
+  font-family: "Montserrat-Bold";
+  text-align: center;
+  border: none;
+  :hover {
+    cursor: pointer;
+  }
+  transition-duration: 0.4s;
+`;
+
+const Div = styled.div`
+  margin-bottom: 15px;
+`;
--- frontend/src/pages/AdminPage/Teams.tsx
+++ frontend/src/pages/AdminPage/Teams.tsx
@@ -1,14 +1,56 @@
 import { Row } from "react-bootstrap";
 import { useTeams } from "../../hooks/team";
 import styled from "styled-components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CreateTeam } from "../../components/Admin/CreateTeam";
 import { Modal } from "../../modal/Modal";
 import image from "../../assets/club.png"
+import { ITeam } from "../../entity/Team";
 
 export function AllTeams() {
   const { teams, loading, error } = useTeams();
   const [createModalActive, setCreateModalActive] = useState(false);
+  const [sortTeamsList, setSortTeams] = useState(teams);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    setSortTeams(teams);
+  }, [teams]);
+
+  function sortRegions(value: string) {
+    if (teams == null) return;
+
+    var buffer = teams;
+
+    if (value == "1")
+      buffer = [].slice
+        .call(teams)
+        .sort((r1: ITeam, r2: ITeam) => (r1.id > r2.id ? 1 : -1));
+
+    if (value == "2")
+      buffer = [].slice
+        .call(teams)
+        .sort((r1: ITeam, r2: ITeam) => (r1.name > r2.name ? 1 : -1));
+
+    setSortTeams(buffer);
+  }
+
+  const Search = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    var text = e.target.value;
+    console.log(text);
+
+    setSearch(text);
+    if (text !== "") {
+      setSortTeams(
+        sortTeamsList.filter(
+          (n) => n.name.toLowerCase().indexOf(text.toLowerCase()) !== -1
+        )
+      );
+    } else {
+      setSortTeams(teams);
+    }
+  };
 
   const toTeam = (id: number) => {
     window.location.assign("/admin/team/calendar?id=" + id);
@@ -19,12 +61,17 @@ export function AllTeams() {
       <Modal active={createModalActive} setActive={setCreateModalActive}>
         <CreateTeam />
       </Modal>
-      <button style={{borderRadius:"3px"}} onClick={() => setCreateModalActive(true)}>
+      <input value={search} onChange={(e) => Search(e)} style={{ marginRight: "20px", borderRadius: "3px" }} placeholder="Поиск команды"/>
+      <select onChange={(e) => sortRegions(e.target.value)}>
+        <option value="2">названию</option>
+        <option value="1">id</option>
+      </select>
+      <button style={{ marginLeft: "20px", borderRadius: "3px" }} onClick={() => setCreateModalActive(true)}>
         Создать команду
       </button>
       {error && <>Ошибка</>}
       {loading && <>Загрузка</>}
-      {teams.map((team) => (
+      {sortTeamsList.map((team) => (
         <RowItem key={team.id} onClick={() => toTeam(team.id)}>
           <img
             src={team.image != null ? team.image : image}
